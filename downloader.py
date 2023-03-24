bce1235b4bc54d33e236e0a5c69ce03edb48e13c
Akshatha Nayak <akshatha@ebi.ac.uk>
2023-03-24 12:16:18 +0000

    multithreading

--- downloader.py
+++ downloader.py
@@ -7,6 +7,7 @@ import getpass
 import argparse
 import requests
 import warnings
+import multiprocessing
 
 def authorize(user, pwd):
     url = "https://api.faang.org/api-token-auth/"
@@ -20,22 +21,16 @@ def authorize(user, pwd):
     return False
 
 def fetch_study_files(study_id, token):
-    url = f"https://api.faang.org/private_portal/file/?size=10000&from_=0&search={study_id}"
+    url = f"https://api.faang.org/private_portal/file/?size=10&from_=0&search={study_id}"
     res = requests.get(url, headers={"Authorization": f"jwt {token}"})
     if res.status_code == 200:
         data = json.loads(res.content)['hits']['hits']
         return data
     return False
 
-def create_directory(filename, directory, parent_dir):
-    path = os.path.join(parent_dir, directory)
-    try:
-        os.makedirs(path, exist_ok=True)
-        local_filename = os.path.join(path, filename)
-        return local_filename
-    except:
-        print(f"Directory {directory} can not be created")
-        return False
+def download_process(filename, url, output_path):
+    print(f"\nDownloading file {filename}")
+    wget.download(url, out=output_path)
 
 def main(user, password, study_id, download_location):
     token = authorize(user, password)
@@ -43,26 +38,31 @@ def main(user, password, study_id, download_location):
         if (download_location == '' or download_location is None) or os.path.exists(download_location) == False:
             print("Download location not provided, using default location")
             download_location = pathlib.Path(__file__).parent.resolve()
-
-        data = fetch_study_files(study_id, token)
-        if data:
-            count = 1
-            total = len(data)
-            for file in data:
-                url = f"ftp://{file['_source']['url']}"
-                filename = file['_source']['name']
-                file_path = create_directory(filename, study_id, download_location)
-                if file_path:
-                    print(f"\n{count}/{total} Downloading file {filename}")
-                    wget.download(url, out=file_path)
-                    count -= 1
-                else:
-                    print(f"Directory {study_id} can not be created")
-            print("Downloading Completed")
-
-        else:
-            print(f"No files found for the study {study_id}")
-            sys.exit(0)
+            download_location = os.path.join(download_location, "data")
+        path = os.path.join(download_location, study_id)
+        try:
+            os.makedirs(path, exist_ok=True)
+            data = fetch_study_files(study_id, token)
+            if data:
+                download_list = []
+                for file in data:
+                    url = f"ftp://{file['_source']['url']}"
+                    filename = file['_source']['name']
+                    file_path = os.path.join(path, filename)
+                    download_list.append([url, filename, file_path])
+                cpus = multiprocessing.cpu_count()
+                max_pool_size = 8
+                pool = multiprocessing.Pool(cpus if cpus < max_pool_size else max_pool_size)
+                for url, filename, path in download_list:
+                    pool.apply_async(download_process, args=(filename, url, path))
+                pool.close()
+                pool.join()
+                print("Download complete")
+            else:
+                print(f"No files found for the study {study_id}")
+                sys.exit(0)
+        except:
+            print(f"Directory {study_id} can not be created")
     else:
         print("Invalid credentials!")
 
